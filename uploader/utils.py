f665435ff979d8eed5841a7b832c9b32fc9a3b7c
Edilson <edilsongudo@gmail.com>
2022-07-04 15:48:40 +0200

    upadte: utils.py

--- uploader/utils.py
+++ uploader/utils.py
@@ -1,10 +1,11 @@
 import os
+from django.conf import settings
 import uuid
 
 
 def handle_incoming_file_chunk(file):
     file_id = file.name
-    temporary_path = os.path.join('media', file_id)
+    temporary_path = os.path.join(settings.MEDIA_ROOT, file_id)
     with open(temporary_path, 'ab') as f:
         f.write(file.read())
     return temporary_path
@@ -12,7 +13,7 @@ def handle_incoming_file_chunk(file):
 
 def handle_last_file_chunk(temporary_path, filename):
     final_filename = filename
-    final_path = os.path.join('media', final_filename)
+    final_path = os.path.join(settings.MEDIA_ROOT, final_filename)
     if os.path.exists(final_path):
         final_filename = (
             os.path.splitext(filename)[0]
@@ -20,7 +21,7 @@ def handle_last_file_chunk(temporary_path, filename):
             + uuid.uuid4().hex[0:3]
             + os.path.splitext(filename)[1]
         )
-        final_path = os.path.join('media', final_filename)
+        final_path = os.path.join(settings.MEDIA_ROOT, final_filename)
     os.rename(temporary_path, final_path)
     return final_path
 
